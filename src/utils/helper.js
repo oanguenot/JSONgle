dbd4fbf864ff71e4b612fd3f4623f05b53c9f7ce
Olivier Anguenot <olivier.anguenot@al-enterprise.com>
2021-04-30 16:38:32 +0200

    Prepare version 1.3.0

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -2,6 +2,6 @@ import { v4 as uuidv4 } from "uuid";
 
 export const getLibName = () => "JSONgle";
 
-export const getVersion = () => "1.2.0";
+export const getVersion = () => "1.3.0";
 
 export const generateNewId = () => uuidv4();
