dc71a18dda5129a35b149bd3650eede83d8fa9be
Olivier Anguenot <olivier.anguenot@al-enterprise.com>
2021-05-27 11:01:54 +0200

    Rework building step with Webpack to be "module" compliant everywhere

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -2,6 +2,6 @@ import { v4 as uuidv4 } from "uuid";
 
 export const getLibName = () => "JSONgle";
 
-export const getVersion = () => "2.3.0";
+export const getVersion = () => "2.3.1";
 
 export const generateNewId = () => uuidv4();
---
+++ webpack.config-prod.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+ module.exports = {
+   entry: './src/index.js',
+   mode: "production",
+   output: {
+     path: path.resolve(__dirname, 'dist'),
+     filename: 'JSONgle.js',
+    library: {
+      name: 'JSONgle',
+      type: 'umd',
+    },
+   },
+ };
