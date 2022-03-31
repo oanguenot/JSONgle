2798c0f91dd618dd1735de63d0b1198dd19d6a64
Olivier Anguenot <oanguenot@gmail.com>
2022-03-31 10:29:10 +0200

    Update version

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -2,6 +2,6 @@ import { v4 as uuidv4 } from "uuid";
 
 export const getLibName = () => "JSONgle";
 
-export const getVersion = () => "3.0.2";
+export const getVersion = () => "3.0.3";
 
 export const generateNewId = () => uuidv4();
